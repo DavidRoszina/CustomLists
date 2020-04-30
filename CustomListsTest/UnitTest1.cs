1c07a55f3cddb1503c37df60da6886cb85926aec
DavidRoszina <david.a.roszina@gmail.com>
2020-04-29 23:07:38 -0500

    Added 3 Remove Tests

--- CustomListsTest/UnitTest1.cs
+++ CustomListsTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace CustomListsTest
     public class UnitTest1
     {
         [TestMethod]
-        
+
         public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
         {
             // arrange
@@ -53,10 +53,10 @@ namespace CustomListsTest
             int itemToAdd3 = 30;
             int expected = 20;
             int actual;
-            
+
 
             //act
-            testList.Add(itemToAdd1); 
+            testList.Add(itemToAdd1);
             testList.Add(itemToAdd2);
             testList.Add(itemToAdd3);
             actual = testList[1];
@@ -87,7 +87,7 @@ namespace CustomListsTest
 
             //assert
             Assert.AreEqual(expected, actual);
-            
+
         }
         [TestMethod]
         public void IncreaseArrayCapacityTest()
@@ -110,5 +110,63 @@ namespace CustomListsTest
             Assert.AreEqual(expected, actual);
 
         }
-    }
+        [TestMethod]
+        public void AddFourPositiveValuesSubtrackOne_CheckCount()
+        {
+            //arange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 3;
+            int actual;
+            //act
+            testList.Add(itemToAdd);
+            testList.Add(itemToAdd);
+            testList.Add(itemToAdd);
+            testList.Add(itemToAdd);
+            testList.Remove(itemToAdd);
+            actual = testList.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AddFourPositiveValuesSubtract2nd_CheckCount()
+        {
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 3;
+            int actual;
+            //act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(itemToAdd2);
+            actual = testList.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void AddFourPositiveValuesSubtract2nd_Check3rdIndex()
+        {
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 3;
+            int actual;
+            //act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(itemToAdd2);
+            actual = (testList[2] = 40);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
 }
